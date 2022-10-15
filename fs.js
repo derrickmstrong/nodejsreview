b4bc9e487e7bd48c95341b80b8be8e4818fead91
D. Strong <64806865+derrickmstrong@users.noreply.github.com>
2022-10-15 08:36:21 -0500

    Reviewing file system (sync) module
    readFileSync
    writeFileSync
    require('fs')

--- fs.js
+++ fs.js
@@ -1,2 +1,14 @@
 // File System
 
+const { readFileSync, writeFileSync } = require('fs');
+
+const first = readFileSync('./content/first.txt', 'utf8');
+const second = readFileSync('./content/second.txt', 'utf8');
+
+console.log(first, second);
+
+writeFileSync(
+    './content/result.txt', // File to write to; file will be created if it does not already exist
+    `Here is the result: ${first}, ${second}. \n`, // Content to write
+    {flag: 'a'} // Set a flag stating that you should append to file (rather than overwrite it)
+)
